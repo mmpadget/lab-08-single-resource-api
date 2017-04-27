2e75e062d2090f79378723e3a6d3d5a68913e3c0
Michael Padget <mpadget@comcast.net>
2017-04-27 12:31:36 -0700

    Writing tests in progress

--- lab-padget/test/live-show-test.js
+++ lab-padget/test/live-show-test.js
@@ -33,53 +33,54 @@ describe('Server module', function() {
         });
       });
     });
+  });
 
 // ### GET TESTS ###
 
-    // TODO: GET - test 200, response body like {<data>} for a request made with a valid id
-    // TODO: GET - test 404, responds with not found for valid request made with an id that was not found
-    describe('/music endpoint', function() {
-      it('should respond with a 200 on proper request', done => {
-        chai.request(server)
-        .post('/music')
-        .send({text: 'hello world'})
-        .end(function(err, res) {
-          if(err) console.error(err.message);
-          console.log('Log of POST 200 ', res.status);
-          expect(res.status).to.equal(200);
-          done();
-        });
-      });
-    });
-  });
+  // TODO: GET - test 200, response body like {<data>} for a request made with a valid id
+  // TODO: GET - test 404, responds with not found for valid request made with an id that was not found
+  // describe('/music endpoint', function() {
+  //   it('should respond with a 200 on proper request', done => {
+  //     chai.request(server)
+  //     .post('/music')
+  //     .send({})
+  //     //.send({artist: 'Lala', album: 'GetReal', song:'MakeBelieve'})
+  //     .end(function(err, res) {
+  //       if(err) console.error(err.message);
+  //       console.log('Log of POST 200 ', res.status);
+  //       expect(res.status).to.equal(200);
+  //       done();
+  //     });
+  //   });
+  // });
 
-  describe('GET method', function() {
-    // describe('/ endpoint', function() {
-    //   it('should respond with a 400 on bad request', done => {
-    //
-    //     done();
-    //   });
-    // });
-    describe('/music endpoint', function() {
-      it('should respond with a 200 on proper request', done => {
-        chai.request(server)
-        .get('/music')
-        .query({text: 'text'})
-        .end(function(err, res) {
-          if(err) console.error(err);
-          console.log('Log of POST 200 ', res.status);
-          // expect(res.status).to.equal(200);
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
-      });
-      // it('should respond with a 400 on bad request', done => {
-      //
-      //   done();
-      // });
-    });
-  });
+  // describe('GET method', function() {
+  //   // describe('/ endpoint', function() {
+  //   //   it('should respond with a 400 on bad request', done => {
+  //   //
+  //   //     done();
+  //   //   });
+  //   // });
+  //   describe('/music endpoint', function() {
+  //     it('should respond with a 200 on proper request', done => {
+  //       chai.request(server)
+  //       .get('/music')
+  //       .query({text: 'text'})
+  //       .end(function(err, res) {
+  //         if(err) console.error(err);
+  //         console.log('Log of POST 200 ', res.status);
+  //         // expect(res.status).to.equal(200);
+  //         expect(err).to.be.null;
+  //         expect(res).to.have.status(200);
+  //         done();
+  //       });
+  //     });
+  //     // it('should respond with a 400 on bad request', done => {
+  //     //
+  //     //   done();
+  //     // });
+  //   });
+  // });
 
   // ### PUT TESTS ###
 
